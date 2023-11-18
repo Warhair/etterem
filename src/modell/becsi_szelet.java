f9fe7d72672e5baa1479d3a3d147d43c80b8cfce
Warhair <tegzesmarton@gmail.com>
2023-11-18 15:22:35 +0100

    etel_megirave

--- src/modell/becsi_szelet.java
+++ src/modell/becsi_szelet.java
@@ -8,6 +8,38 @@ package modell;
  *
  * @author dobrocsi.kornel
  */
-public class becsi_szelet {
+public class Becsi_szelet  extends Etel{
+    private enum Meretek {URES, NEGYED_TAL, FEL_TAL, HAROMNEGYED_TAL, EGESZ_TAL}
+    private Meretek meret;
+    
+    public Becsi_szelet(String etel_nev, Meretek meret){
+        super(etel_nev);
+       this.meret = Meretek.EGESZ_TAL;
+    }   
+    public Becsi_szelet(String etel_nev, boolean megrendelve) {
+        super(etel_nev, megrendelve);
+        this.meret = meret;
+    }
+    public void osszemegy(){
+        /* IDE ajánlása: */
+        meret = switch (meret) {
+            case EGESZ_TAL -> Meretek.HAROMNEGYED_TAL;
+            case HAROMNEGYED_TAL -> Meretek.FEL_TAL;
+            case FEL_TAL -> Meretek.NEGYED_TAL;
+           case NEGYED_TAL -> Meretek.URES;
+           default -> Meretek.URES;
+        };
+    }
+    
+    @Override
+    public void altalanosSutesHatasa() {
+        if(meret.ordinal() > 0){
+            meret = Meretek.values()[meret.ordinal() - 1];
+        }
+    }
+
+    @Override
+    public void kimeloSutesHatasa() {
+    }
     
 }
--- src/modell/etel.java
+++ src/modell/etel.java
@@ -2,8 +2,44 @@
 package modell;
 
 
-public abstract class etel {
+public abstract class Etel {
     private String etel_nev;
     private boolean megrendelve;
-      
+    
+    public Etel(String etel_nev){
+        this.etel_nev = etel_nev;
+        this.megrendelve = true;
+    }
+    
+    public Etel(String etel_nev, boolean megrendelve) {
+        this.etel_nev = etel_nev;
+        this.megrendelve = megrendelve;
+    }
+
+    public String getEtel_nev() {
+        return etel_nev;
+    }
+
+    public void setEtel_nev(String etel_nev) {
+        this.etel_nev = etel_nev;
+    }
+
+    public boolean isMegrendelve() {
+        return megrendelve;
+    }
+
+    public void setMegrendelve(boolean megrendelve) {
+        this.megrendelve = megrendelve;
+    }
+         
+
+    @Override
+    public String toString() {
+        return "etel{" + "etel_nev=" + etel_nev + ", megrendelve=" + megrendelve + '}';
+    }
+    
+    
+    public abstract void altalanosSutesHatasa();
+    public abstract void kimeloSutesHatasa();
+    
 }
